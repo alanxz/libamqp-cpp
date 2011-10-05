db19d7944d20069ccd8ef36c0b7aa9b7601f4760
Alan Antonuk <alan.antonuk@gmail.com>
2011-10-05 01:33:51 -0400

    Fixing compile errors in exception defn

--- src/exception.h
+++ src/exception.h
@@ -3,4 +3,23 @@
 
 #include <stdexcept>
 
+namespace amqpp
+{
+
+class channel_exception : public std::runtime_error
+{
+public:
+  explicit channel_exception() throw() : std::runtime_error("A channel exception occurred") {}
+  virtual ~channel_exception() throw() {}
+
+};
+
+class connection_exception : public std::runtime_error
+{
+public:
+  explicit connection_exception() throw() : std::runtime_error("A connection exception occurred") {}
+  virtual ~connection_exception() throw() {}
+};
+
+}
 #endif // _AMQPP_EXCEPTION_H_INCLUDED_
